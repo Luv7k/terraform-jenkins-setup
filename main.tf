2a061d6a1cf4d152c18e694562fc3904082a7d2d
Lovesh Bishnoi <bishno01@ads.uni-passau.de>
2024-03-12 15:58:19 +0100

    SG added for Jenkins on EC2

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ module "networking" {
   private_subnet_cidr  = var.private_subnet_cidr
   us_availability_zone = var.us_availability_zone
 }
+
+module "security_group" {
+  source              = "./security-groups"
+  vpc_id              = module.networking.dev_proj_1_vpc_id
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTP(80) and HTTPS(443)"
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+}
